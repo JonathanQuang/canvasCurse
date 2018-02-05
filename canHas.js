e22d456dfa56b575b1c27c4224ca19d7972a9e81
JonathanQuang <jquang@stuy.edu>
2018-02-05 13:57:38 -0500

    lines connecting shapes

--- canHas.js
+++ canHas.js
@@ -2,24 +2,49 @@ console.log("loaded");
 var canvasElement = document.getElementById("slate");
 var canvasContext = canvasElement.getContext("2d");
 var shape = "square";
+var lastPoint = [-1,-1];
 
 
 
 
 var drawSquare = function(event){
 	console.log(event);
+	theX = event.offsetX;
+	theY = event.offsetY;
+	
 	if (shape == "square"){
-		canvasContext.fillRect(event.x,event.y,50,50);
+		canvasContext.fillRect(theX,theY,50,50);
 	}
 	else if (shape == "circle"){
 		canvasContext.beginPath();
-		canvasContext.arc(event.x,event.y,25,0,2*Math.PI);
+		canvasContext.arc(theX,theY,25,0,2*Math.PI);
 		canvasContext.stroke();
 		canvasContext.fill();
+		//theX += -25;
+		//theY += -25;
+	}
+	
+	if (lastPoint[0]!=-1 && lastPoint[1]!=-1){
+		canvasContext.moveTo(lastPoint[0],lastPoint[1]);
+		if (shape=="square"){
+			canvasContext.lineTo(theX+25,theY+25);
+		}
+		else{
+			canvasContext.lineTo(theX,theY);
+		}
+		canvasContext.stroke();
+	}
+	
+	if (shape == "square"){
+		lastPoint=[event.offsetX+25,event.offsetY+25];
+	}
+	else {
+		lastPoint = [theX,theY];
 	}
 }
 
 var clearCanvas =function(){
+	lastPoint = [-1,-1];
 	canvasContext.clearRect(0,0,500,500);
 }
 
