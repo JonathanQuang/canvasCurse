d97223195641e364eaee14a9c9e437d01ee539d3
JonathanQuang <jquang@stuy.edu>
2018-02-04 21:18:35 -0500

    added all the stuff

--- canHas.js
+++ canHas.js
@@ -1,19 +1,35 @@
 console.log("loaded");
 var canvasElement = document.getElementById("slate");
 var canvasContext = canvasElement.getContext("2d");
+var shape = "square";
 
 
 
 
 var drawSquare = function(event){
 	console.log(event);
-	canvasContext.fillRect(event.x,event.y,50,50);
-
+	if (shape == "square"){
+		canvasContext.fillRect(event.x,event.y,50,50);
+	}
+	else if (shape == "circle"){
+		canvasContext.beginPath();
+		canvasContext.arc(event.x,event.y,25,0,2*Math.PI);
+		canvasContext.stroke();
+		canvasContext.fill();
+	}
 }
 
 var clearCanvas =function(){
 	canvasContext.clearRect(0,0,500,500);
 }
 
+var toggleShape = function(){
+	if (shape=="square"){
+		shape="circle";
+	}
+	else if (shape == "circle"){
+		shape = "square";
+	}
+}
 
 canvasElement.addEventListener("click",drawSquare);
